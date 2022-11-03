e72935e52766ee725845954c23f5b872d541b297
emazou <estefania.mazou@gmail.com>
2022-11-03 17:10:13 -0500

    hook added

--- src/pages/predios/[id].js
+++ src/pages/predios/[id].js
@@ -1,22 +1,50 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { GET_PREDIO } from 'lib/graphql/queries/predios.query';
-import { useQuery } from '@apollo/client';
+import { Card, Modal } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import EditPredio from '@components/Predios/EditPredio';
+import { editPredioOpenModal } from 'redux/modalSlice';
+import { editPredio } from 'redux/editSlice';
+import Propietarios from '@components/Propietarios/Propietarios';
+import { useGetPredioQuery } from 'redux/prediosAPI';
+import { useDispatch, useSelector } from 'react-redux';
+
 export default function PredioPage() {
     const router = useRouter()
     const { id } = router.query
-    const { loading, error, data } = useQuery(GET_PREDIO, {
-        variables: { id: `${id}` }
-    })
-    const predio = data?.predios[0]
-    console.log(data)
+    const dispatch = useDispatch();
+    const isOpenModal = useSelector((state) => state.modal.isOpenEditModal);
+    const { loading, data, refetch } = useGetPredioQuery(id)
+    const predio = data?.data.predios[0]
     return (
         <div className='container flex-column'>
             {
-                loading && <div className="ping"></div>
-            }
-            {id}
+                loading
+                    ?
+                    <div className="ping"></div>
+                    :
+                    <Card
+                        title={predio?.nombre}
+                        style={{ width: '100%' }}
+                        actions={[
+                            <EditOutlined key="edit"
+                                onClick={() => {
+                                    dispatch(editPredioOpenModal())
+                                    dispatch(editPredio({ avaluo: predio?.avaluo, nombre: predio?.nombre, departamento: predio?.departamento, municipio: predio?.municipio, id: id, noPredial: predio.no_predial }))
+                                }}
 
+                            />
+                        ]}
+                    >
+                        <p>Número Predial: {predio?.nopredial}</p>
+                        <p>{predio?.municipio} - {predio?.departamento}</p>
+                        <h4>Avaluo: ${predio?.avaluo}</h4>
+                    </Card>
+            }
+            <Modal title="Formulario de edición de predios" footer={null} open={isOpenModal} onCancel={() => dispatch(editPredioOpenModal())}>
+                <EditPredio fn={() => refetch()} id={id} nombre={predio?.nombre} municipio={predio?.municipio} departamento={predio?.departamento} avaluo={predio?.avaluo} noPredial={predio?.nopredial} />
+            </Modal>
+            <Propietarios id={id} />
         </div>
     )
 }
