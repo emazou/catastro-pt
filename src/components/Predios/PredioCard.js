496e09686092310c4498c9690e4b038f06e11b55
emazou <estefania.mazou@gmail.com>
2022-11-03 17:11:24 -0500

    hook added

--- src/components/Predios/PredioCard.js
+++ src/components/Predios/PredioCard.js
@@ -1,17 +1,12 @@
-import React, { use } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { Card, Modal } from 'antd';
-import { RightOutlined, EditOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
-import { useDeletePredio } from '../../hooks/useMutationPredio';
-import { useDispatch, useSelector } from 'react-redux';
-import EditPredio from './EditPredio';
-import { editPredioOpenModal } from 'redux/modalSlice';
+import { ArrowRightOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import { useDeletePredioMutation } from 'redux/prediosAPI';
+import { toast } from 'react-hot-toast';
 const { confirm } = Modal;
-
-export default function PredioCard({ avaluo, nombre, departamento, municipio, id }) {
-    const deletePredio = useDeletePredio();
-    const dispatch = useDispatch();
-    const isOpenModal = useSelector((state) => state.modal.isOpenEditModal)
+export default function PredioCard({ avaluo, nombre, departamento, municipio, id, noPredial, fn }) {
+    const [deletePredio] = useDeletePredioMutation();
     const showPromiseConfirm = () => {
         confirm({
             title: 'Quieres eliminar este predio?',
@@ -19,42 +14,36 @@ export default function PredioCard({ avaluo, nombre, departamento, municipio, id
             okText: 'Eliminar',
             cancelText: 'Cancelar',
             onOk() {
-                deletePredio(
-                    {
-                        variables: {
-                            id: id
+                deletePredio(id)
+                    .then((res) => {
+                        if (res.data?.data) {
+                            fn()
+                            toast.success('Se eliminó el predio')
+                        } else {
+                            toast.error('No se pudo eliminar el predio')
                         }
-                    },
-                )
+                    })
+                    .catch((error) => console.log(error))
             },
             onCancel() { },
         });
     };
     return (
-        <div>
-            <Modal title="Formulario de edición de predios" footer={null} open={isOpenModal} onCancel={() => dispatch(editPredioOpenModal())}>
-                <EditPredio id={id} nombre={nombre} municipio={municipio} departamento={departamento} avaluo={avaluo} />
-            </Modal>
-            <Card
-                title={nombre}
-                extra={<Link
-                    className='link-detail'
-                    href={`/predios/${id}`}>Ver detalles <RightOutlined /></Link>}
-                style={{ width: 300, }}
-                actions={[
-                    <EditOutlined key="edit"
-                        onClick={() => dispatch(editPredioOpenModal())}
-
-                    />,
-                    <DeleteOutlined key="delete"
-                        onClick={showPromiseConfirm}
-                    />,
-                ]}
-            >
-                <p>No.Predial {id}</p>
-                <p>{municipio} - {departamento}</p>
-                <h3>Avaluo: ${avaluo}</h3>
-            </Card>
-        </div>
+        <Card
+            title={nombre}
+            extra={<Link
+                className='link-detail'
+                href={`/predios/${id}`}>Ver detalles <ArrowRightOutlined /></Link>}
+            style={{ width: 300, }}
+            actions={[
+                <DeleteOutlined key="delete"
+                    onClick={showPromiseConfirm}
+                />,
+            ]}
+        >
+            <p className='fw'>No.Predial: {noPredial}</p>
+            <p>{municipio} - {departamento}</p>
+            <p>Avalúo: ${avaluo} COP</p>
+        </Card>
     )
 }
