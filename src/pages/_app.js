45ad5893e3bd07a9482306865c0a99ea638af665
emazou <estefania.mazou@gmail.com>
2022-11-03 23:44:01 -0500

    apollo client deleted

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,15 @@
 import 'antd/dist/antd.css';
-import 'styles/globals.css'
-import { ApolloProvider } from '@apollo/client';
+import 'styles/globals.css';
 import { Provider } from 'react-redux';
 import MainLayout from 'layout/MainLayout';
-import client from '../lib/apollo_client';
-import store from 'redux/store';
+import store from 'features/store';
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </Provider>
-    </ApolloProvider>
+    <Provider store={store}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Provider>
   )
 }
 
